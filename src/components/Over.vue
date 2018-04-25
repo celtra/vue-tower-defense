e451fac3b408d6a60e7a095d93e9a4c302ced392
zan <zan.kusterle@gmail.com>
2018-04-25 13:56:34 +0200

    9-router

---
+++ src/components/Over.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h1 class="over-text" v-if="won">You won!</h1>
+		<h1 class="over-text" v-else>You lost!</h1>
+
+		<div class="info">
+			<button @click="$router.push({ name: 'Game' })">Play again</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	computed: {
+		won () {
+			return this.$route.params.won
+		}
+	}
+}
+</script>
+
+<style>
+@import '../main.css';
+</style>
