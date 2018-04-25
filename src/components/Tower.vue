43a55e364e556b567b28cb40b1b8e645cf28feaa
zan <zan.kusterle@gmail.com>
2018-04-25 13:22:10 +0200

    3-tower-health

--- src/components/Tower.vue
+++ src/components/Tower.vue
@@ -1,5 +1,26 @@
 <template>
-    <div class="tower">
-        This will be a tower
+    <div class="tower" :class="{'tower-dead': !isAlive}">
+        <progress class="health" :value="health" max="100"></progress>
     </div>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            health: 100
+        }
+    },
+    computed: {
+        isAlive () {
+            return this.health > 0
+        },
+        damage () {
+            return 10
+        },
+        farm () {
+            return 20
+        }
+    }
+}
+</script>
